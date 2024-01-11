e002f0d6d96bb8d4681891111b8ec5ae2cdf5aa6
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-01-11 10:59:39 -0700

    added uppercase function, and moved some starter code

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
 // Assignment code here
-var passwordLength;
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// moved var generateBtn to top to organize my own code. 
+var passwordLength;
+var numberCheck;
+var uppercaseABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var uppercaseCheck; 
+var lowercaseABE = "abcdefghijklmnopqrstuvwxyz";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+
+  // This will be the length of the password critera 
   function determineLength(){
     passwordLength = prompt ("how long the password should be?");
-  // legnth of password? 8-50?
+
   if (passwordLength<8){
-    alert("Password Length much be a number between 8-? characters.");
+    alert("Password Length much be a number between 8-50 characters.");
   
   }else if (passwordLength>50){
-    alert("Password length much be a number between 8-? characters.");
+    alert("Password length much be a number between 8-50 characters.");
   
   // }else if no number is prsented
-    alert ("Password length much be a number between 8-? characters.");
+    alert ("Password length much be a number between 8-50 characters.");
   
   }else{
       alert("Next steps")
     }
     return passwordLength;
   }
+
+// This will be for the letters in the password
+
+
+
+  function determineUppercase(){
+    uppercaseCheck = prompt ("Would you like to use an uppercase letter?")
+
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Yes or No");
+      determineUppercase();
+
+    }else if (uppercaseCheck === "yes" || uppercaseCheck === "y"){
+      numberCheck = true;
+      return numberCheck;
+
+    }else if (uppercaseCheck === "no" || uppercaseCheck === "n"){
+      numberCheck = false;
+      return numberCheck;
+  
+    }else {
+      alert("Please answer Yes or No");
+      determineUppercase();
+    }
+    return uppercaseCheck;
+  }
+
+
+
+  // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
+// Moved to bottom? This is last step 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
