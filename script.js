4e53e2cb180f9d778f68f022cfdbf667382e1b67
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-01-10 18:30:00 -0700

    started writting functions for adding in characters

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-
+var passwordLength;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +10,25 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  function determineLength(){
+    passwordLength = prompt ("how long the password should be?");
+  // legnth of password? 8-50?
+  if (passwordLength<8){
+    alert("Password Length much be a number between 8-? characters.");
+  
+  }else if (passwordLength>50){
+    alert("Password length much be a number between 8-? characters.");
+  
+  // }else if no number is prsented
+    alert ("Password length much be a number between 8-? characters.");
+  
+  }else{
+      alert("Next steps")
+    }
+    return passwordLength;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
